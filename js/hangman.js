c74887172fc0a8c9756e7c326ec7a47f897e7998
danyallsey04@gmail.com <NAME@example.coml.com>
2023-08-01 06:09:21 -0700

    update mistake created

--- js/hangman.js
+++ js/hangman.js
@@ -24,10 +24,11 @@ let answer ="";
 let maxWrong =6 ;
 let mistakes= 0;
 let guessed =[]
+let wordStatus= null
 
 function randomWords(){
 
-  return  answer = programing_languages[Math.floor(Math.random() * programing_languages.length )]
+    answer = programing_languages[Math.floor(Math.random() * programing_languages.length )]
 
 }
 
@@ -35,7 +36,7 @@ function generateButton(){
 
     let buttons= `ABCDEFGHIJKLMNOPQRSTUVWXYZ`.split('').map((item)=>
         `
-        <button id="`+item+`" class="bg-blue-200 m-2 py-2 px-3 rounded border-[2px] border-slate-400 hover:shadow-slate-500 shadow-lg" onclick="handleguess('`+item+ `')" >
+        <button id= '`+item+`' class="bg-blue-200 m-2 py-2 px-3 rounded border-[2px] border-slate-400 hover:shadow-slate-500 shadow-lg" onclick="handleguess('`+item+ `')" >
         `+item+`
         <button/>
 
@@ -46,22 +47,47 @@ function generateButton(){
     document.getElementById('all-buttons').innerHTML= buttons.join('')
 
 }
+
 function handleguess(chosenLetter){
 
     guessed.indexOf(chosenLetter) === -1 ? guessed.push(chosenLetter) : null
 
-    document.getElementById(chosenLetter)
+  //  document.getElementById(chosenLetter).setAttribute('disabled',true)
 
+  console.log(answer);
     if(answer.indexOf(chosenLetter) >= 0 ){
 
         guessWord()
 
+    }else if(answer.indexOf(chosenLetter) >= -1 ){
+
+        mistakes++
+        updateMistake()
+
     }
+
+
 
     
 }
 
 
 
+function updateMistake(){
+    document.getElementById("mistakes").innerHTML = mistakes
+}
+
+
+function guessWord(){
+
+    wordStatus = answer.split('').map((item)=> guessed.indexOf(item) >= 0 ? item : '_').join('')
+
+    document.getElementById('wordStatus').innerHTML= wordStatus
+    console.log(wordStatus);
+
+
+}
 
 generateButton()
+handleguess()
+randomWords()
